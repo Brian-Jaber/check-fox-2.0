bcff858aa79f07183c25bba127a13f70c76fc405
Brian Jaber <brianjaber@gmail.com>
2023-03-30 12:07:14 -0700

    Worked through JSX section

--- src/Pet.jsx
+++ src/Pet.jsx
@@ -1,11 +1,12 @@
-import React from "react";
-
 const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <h2>{props.animals}</h2>
+      <h2>{props.breed}</h2>
+    </div>
+  );
 };
 
+// Related to ES6, general top level export
 export default Pet;
