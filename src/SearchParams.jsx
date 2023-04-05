85f323e2f5afd2bc12e952d29ac2c5516c546949
Brian Jaber <brianjaber@gmail.com>
2023-04-05 11:45:04 -0700

    Brief refresher on the importance and nuances of using map to map our array of strings to an array of components.

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
+const ANIMALS = ["bird", "cat", "rabbit", "reptile"];
 
 const SearchParams = () => {
   const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
   // Lines 6-8 are equivalent to line 4.  These explain what is happening under the hood
   // const locationHook = useState("");
   // const location = locationHook[0];
@@ -18,6 +20,21 @@ const SearchParams = () => {
             value={location}
             placeholder="Location"
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => {
+              setAnimal(e.target.value);
+            }}
+          >
+            <option />
+            {ANIMALS.map((animal) => (
+              <option key={animal}>{animal}</option>
+            ))}
+          </select>
         </label>
       </form>
     </div>
