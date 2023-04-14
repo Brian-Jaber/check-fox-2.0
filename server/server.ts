ed433dc13fd500ffb4e303901bba81e1d72a4ca3
Brian Jaber <brianjaber@gmail.com>
2023-04-14 15:09:01 -0700

    Loaded dotenv, cors, and mysql packages in server.ts file. Also called express.json() middleware.

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,20 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+
+const db = mysql.createPool({
+  host: "localhost",
+  user: "BERRYS",
+  password: process.env.MYSQL_PASSWORD,
+  database: "check_fox",
+});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express server is up and running!");
