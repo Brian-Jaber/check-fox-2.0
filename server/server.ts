63fe8690f423f60015203671681012627dd77a84
Brian Jaber <brianjaber@gmail.com>
2023-04-14 16:45:00 -0700

    Created routes directory inside of server folder.  Created database folder as well as a db.ts file to establish connection to database.

--- server/server.ts
+++ server/server.ts
@@ -3,6 +3,8 @@ import cors from "cors";
 import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 
+dotenv.config();
+
 const db = mysql.createPool({
   host: "localhost",
   user: "BERRYS",
