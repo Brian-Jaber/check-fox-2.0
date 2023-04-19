aac363b00c865d3886a31f306c02d96ea96eb09a
Brian Jaber <brianjaber@gmail.com>
2023-04-19 16:00:50 -0700

    Delete comparePasswords.ts file, initially was going to modularize in the case that I needed to reuse it later on.  However the code is short and if need be such a fix could be easily implemented later on
    typescript was giving an error as password was implied type any.
    installed bcrypt/type package

--- utilities/passwordUtils/hashpassword.ts
+++ utilities/passwordUtils/hashpassword.ts
@@ -1,10 +1,9 @@
 import bcrypt from "bcrypt";
 
-async function hashPassword(password) {
+async function hashPassword(password: string): Promise<string> {
   const saltRounds = 10;
   const hashedPassword = await bcrypt.hash(password, saltRounds);
   return hashedPassword;
 }
 
 export default hashPassword;
-``;
