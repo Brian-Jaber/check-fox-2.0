5898ccfa49fea73887acfea46c810fec0d75ccd7
Brian Jaber <brianjaber@gmail.com>
2023-04-21 16:02:10 -0700

    Small fix, deleted unneccessary import statements inside of server.ts

--- server/server.ts
+++ server/server.ts
@@ -2,8 +2,6 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 //placing db import here but not used as of now, remove later
 // CAN GET RID OF MYSQL BUT KEEPING FOR NOW IN CASE
-import db from "./database/db";
-import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 import authRouter from "./routes/authRoutes";
 
