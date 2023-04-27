02d4ac1222306a715339aa59882bebb757de8c3c
Brian Jaber <brianjaber@gmail.com>
2023-04-26 18:25:39 -0700

    currently implementing proper error handling in regards to mySQL errors.  It seems I need to create a custom type so that the error is properly caught and logged.  At this point I am still geting a 201 response even when creating a duplicate user.  The error message is logging in the terminal but I am still getting a 201 on postman regardless of whether or not a user is created.

--- server/controllers/authController.ts
+++ server/controllers/authController.ts
@@ -1,13 +1,19 @@
 import { Request, Response } from "express";
 import { registerUser } from "../../utilities/passwordUtils/index";
+import { SqlError } from "mysql2";
 
 export async function register(req: Request, res: Response) {
   const { email, first_name, last_name, password } = req.body;
   try {
     await registerUser(email, first_name, last_name, password);
-    console.log(email);
     res.status(201).json({ message: "User registered successfully" });
-  } catch (error) {
-    res.status(500).json({ message: "Error registering user.", error });
+  } catch (error: SqlError) {
+    if (error.code === "ER_DUP_ENTRY") {
+      res
+        .status(409)
+        .json({ message: "Error register user. Email already exists.", error });
+    } else {
+      res.status(500).json({ message: "Error registering user.", error });
+    }
   }
 }
