f36df3b393b27a20d2f89e6b47f71e2fde3adad7
Brian Jaber <brianjaber@gmail.com>
2023-05-31 22:34:28 -0700

    Planning out all test cases for registerUser function, must write tests for current functions before proceeding according to best practices

---
+++ __tests__/utilities/passwordUtils/registerUser.test.ts
@@ -0,0 +1,13 @@
+import {
+  registerUser,
+  isSqlError,
+} from "../../../utilities/passwordUtils/registerUser";
+
+import db from "../../../server/database/db";
+
+// TODO: write tests for all cases of registerUser function
+// Mock Dependencies
+
+//  Describe registerUser function
+// clear mocks before each test
+// delete newly created user after each test case
