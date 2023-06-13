c98a0551a0abbdfe56a26d14f3fd50055bc037d9
Brian Jaber <brianjaber@gmail.com>
2023-06-12 17:48:59 -0700

    Finished registerUser testing for both SqlErrors and unknown errors

--- __tests__/utilities/passwordUtils/registerUser.test.ts
+++ __tests__/utilities/passwordUtils/registerUser.test.ts
@@ -4,6 +4,7 @@ import {
 } from "../../../utilities/passwordUtils/registerUser";
 import db from "../../../server/database/db";
 import hashPassword from "../../../utilities/passwordUtils/hashPassword";
+import { SqlError } from "../../../utilities/customTypes";
 // TODO: write tests for all cases of registerUser function
 // Mock Dependencies
 jest.mock("../../../server/database/db", () => ({
@@ -35,5 +36,66 @@ describe("registerUser", () => {
       [email, first_name, last_name, hashedPassword]
     );
   });
-  // TODO Finish writing afterEach once deleteUser testing and function are done
+
+  it("should log sqlErrors", async () => {
+    const email = "test.email@gmail.com";
+    const first_name = "john";
+    const last_name = "doe";
+    const password = "password";
+    const hashedPassword = "hashed_password";
+    const sqlError: SqlError = {
+      code: "SQL_ERROR",
+      message: "Error occurred in the SQL query.",
+      errno: 1001,
+      sqlState: "",
+      sqlMessage: "Error",
+    };
+
+    (hashPassword as jest.Mock).mockReturnValue(
+      Promise.resolve(hashedPassword)
+    );
+
+    (db.query as jest.Mock).mockReturnValue(Promise.reject(sqlError));
+
+    const consoleSpy = jest.spyOn(console, "error");
+
+    try {
+      await registerUser(email, first_name, last_name, password);
+    } catch (error) {
+      expect(error).toBe(sqlError);
+    }
+
+    expect(consoleSpy).toHaveBeenCalledWith(
+      "Error registering user: ",
+      sqlError
+    );
+  });
+
+  it("Should log unknown errors", async () => {
+    const email = "test.email@gmail.com";
+    const first_name = "john";
+    const last_name = "doe";
+    const password = "password";
+    const hashedPassword = "hashed_password";
+
+    (hashPassword as jest.Mock).mockReturnValue(
+      Promise.resolve(hashedPassword)
+    );
+
+    (db.query as jest.Mock).mockImplementation(() => {
+      throw new Error("not a SqlError");
+    });
+
+    try {
+      await registerUser(email, first_name, last_name, password);
+    } catch (error) {
+      expect(error).toEqual({
+        code: "UNKNOWN_ERROR",
+        message: "An unknown error occurred while registering the user.",
+        errno: -1,
+        sqlState: "",
+        sqlMessage: "",
+      });
+    }
+  });
 });
