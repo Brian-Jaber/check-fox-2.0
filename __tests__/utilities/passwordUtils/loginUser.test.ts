b4d3bdf9e0cf0b7d83350c737fbf000565f40d5a
Brian Jaber <brianjaber@gmail.com>
2023-06-20 23:01:26 -0700

    Made loginUserTest more DRY with variable assignment. Currently, all written tests are passing.

--- __tests__/utilities/passwordUtils/loginUser.test.ts
+++ __tests__/utilities/passwordUtils/loginUser.test.ts
@@ -22,13 +22,13 @@ describe("loginUser", () => {
 
   beforeEach(() => {
     email = "test@test.com";
+    password = "password";
 
     jest.clearAllMocks();
   });
 
   it("Should throw a LoginError when email field is empty.", async () => {
-    const email = "";
-    const password = "password";
+    email = "";
 
     await expect(loginUser(email, password)).rejects.toThrow(
       new LoginError("Please enter email.")
@@ -36,7 +36,6 @@ describe("loginUser", () => {
   });
 
   it("Should throw a LoginError if password field is empty.", async () => {
-    const email = "test@test.com";
     const password = "";
 
     await expect(loginUser(email, password)).rejects.toThrow(
@@ -45,9 +44,6 @@ describe("loginUser", () => {
   });
 
   it("Should throw an errror if incorrectly formatted email.", async () => {
-    const email = "test.@testcom";
-    const password = "password";
-
     (isEmail as jest.Mock).mockReturnValue(false);
 
     await expect(loginUser(email, password)).rejects.toThrow(
@@ -56,9 +52,6 @@ describe("loginUser", () => {
   });
 
   it("It should throw an error if a user does not have a hashed password", async () => {
-    const email = "test.@test.com";
-    const password = "password";
-
     (isEmail as jest.Mock).mockReturnValue(true);
 
     (db.query as jest.Mock).mockResolvedValue([
@@ -76,8 +69,20 @@ describe("loginUser", () => {
   });
 
   it("Should throw an error if the password entered is incorrect.", async () => {
-    const email = "test@test.com";
-    const password = "password";
+    (db.query as jest.Mock).mockResolvedValue([
+      [
+        {
+          email,
+          hashed_password: "hashed_password",
+        },
+      ],
+    ]);
+
+    (bcrypt.compare as jest.Mock).mockResolvedValue(false);
+
+    await expect(loginUser(email, password)).rejects.toThrow(
+      new LoginError("Incorrect password.")
+    );
   });
 
   test.todo("It should return true if the entered password is correct");
